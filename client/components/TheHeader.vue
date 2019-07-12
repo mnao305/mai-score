33797f232d32597d8ffbd0ff168463aff6f4ef8d
mnao305 <naomasa305@gmail.com>
2019-07-12 18:12:56 +0900

    add: ヘッダー
    見た目だけ

---
+++ client/components/TheHeader.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-toolbar clipped-left color="cyan lighten-4" app>
+    <v-toolbar-title style="cursor: pointer;" @click="$router.push('/')"
+      >舞スコア</v-toolbar-title
+    >
+    <v-spacer />
+    <v-menu offset-y>
+      <v-btn slot="activator" icon>
+        <v-icon>more_vert</v-icon>
+      </v-btn>
+      <v-list>
+        <v-list-tile @click="$router.push('/about')">
+          <v-list-tile-title>About</v-list-tile-title>
+        </v-list-tile>
+        <v-list-tile>
+          <v-list-tile-title>Logout</v-list-tile-title>
+        </v-list-tile>
+      </v-list>
+    </v-menu>
+  </v-toolbar>
+</template>
--- client/layouts/default.vue
+++ client/layouts/default.vue
@@ -2,8 +2,19 @@
   <v-app>
     <v-content>
       <v-container>
+        <the-header />
         <nuxt />
       </v-container>
     </v-content>
   </v-app>
 </template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+@Component({
+  components: {
+    TheHeader: () => import('~/components/TheHeader.vue')
+  }
+})
+export default class IndexLayout extends Vue {}
+</script>
