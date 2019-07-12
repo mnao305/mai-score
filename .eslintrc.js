fbf8ef765301fc44e89d8daf0902d604630ed486
mnao305 <naomasa305@gmail.com>
2019-07-12 16:47:43 +0900

    init project

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  parserOptions: {
+    parser: '@typescript-eslint/parser'
+  },
+  extends: [
+    '@nuxtjs',
+    'prettier',
+    'prettier/vue',
+    'plugin:prettier/recommended',
+    'plugin:nuxt/recommended'
+  ],
+  plugins: [
+    '@typescript-eslint',
+    'prettier'
+  ],
+  // add your custom rules here
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'error'
+  }
+}
