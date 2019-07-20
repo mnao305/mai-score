9199cfa8bcc0b9dca145dcc30d7cc6cef3b4cd53
mnao305 <naomasa305@gmail.com>
2019-07-21 01:14:42 +0900

    ログイン・登録ページへのリンク設置

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,5 +1,25 @@
 <template>
-  <div></div>
+  <div id="indexPage">
+    <h2>舞スコア</h2>
+    <p>maimaiでらっくす用のスコア管理ツールです。</p>
+    <v-btn
+      class="loginRegistBtn"
+      outline
+      color="cyan darken-4"
+      @click="$router.push('/login')"
+    >
+      ログイン
+    </v-btn>
+    <br />
+    <v-btn
+      class="loginRegistBtn"
+      outline
+      color="cyan darken-4"
+      @click="$router.push('/regist')"
+    >
+      登録
+    </v-btn>
+  </div>
 </template>
 
 <script lang="ts">
@@ -7,3 +27,12 @@ import { Component, Vue } from 'vue-property-decorator'
 @Component
 export default class IndexPage extends Vue {}
 </script>
+
+<style lang="scss" scoped>
+#indexPage {
+  text-align: center;
+  .loginRegistBtn {
+    width: 200px;
+  }
+}
+</style>
