bed43749dbb7b16c3784838b28babdcf8b6a7659
mnao305 <naomasa305@gmail.com>
2019-07-26 02:12:49 +0900

    認証状態でのページ制御修正

--- client/layouts/default.vue
+++ client/layouts/default.vue
@@ -18,12 +18,28 @@ import auth from '~/plugins/auth'
   }
 })
 export default class IndexLayout extends Vue {
-  async created() {
+  async beforeCreate() {
     const data = await auth.auth()
     if (data) {
-      this.$store.dispatch('user/setUser', data)
+      await this.$store.dispatch('user/setUser', data)
+    } else {
+      await this.$store.dispatch('user/logout')
+    }
+
+    if (
+      this.$store.state.user.isAuthenticatedFlg &&
+      this.$store.state.user.uid
+    ) {
+      if (
+        this.$route.name === 'login' ||
+        this.$route.name === 'regist' ||
+        this.$route.name === 'index'
+      ) {
+        this.$router.push('/myscore')
+      }
     } else {
       this.$store.dispatch('user/logout')
+      if (this.$route.name === 'myscore') this.$router.push('/')
     }
   }
 }
