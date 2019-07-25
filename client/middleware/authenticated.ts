3c63bbe1ab3f42180bc38eae094e27eb17286fd8
mnao305 <naomasa305@gmail.com>
2019-07-25 23:31:06 +0900

    認証時にはindexページに飛べないようにする

--- client/middleware/authenticated.ts
+++ client/middleware/authenticated.ts
@@ -1,6 +1,12 @@
 export default function({ route, store, redirect }) {
   if (store.state.user.isAuthenticatedFlg && store.state.user.uid) {
-    if (route.name === 'login' || route.name === 'regist') redirect('/myscore')
+    if (
+      route.name === 'login' ||
+      route.name === 'regist' ||
+      route.name === 'index'
+    ) {
+      redirect('/myscore')
+    }
   } else {
     store.dispatch('user/logout')
     if (route.name === 'myscore') redirect('/')
