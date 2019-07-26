2d4bb4c9c5f7077113eef522c12aeca73fbcd9e2
mnao305 <naomasa305@gmail.com>
2019-07-26 11:08:16 +0900

    cookieに入ってる情報も見に行くようにする

--- client/middleware/authenticated.ts
+++ client/middleware/authenticated.ts
@@ -1,5 +1,13 @@
-export default function({ route, store, redirect }) {
-  if (store.state.user.isAuthenticatedFlg && store.state.user.uid) {
+import * as Cookies from 'js-cookie'
+import cookie from 'cookie'
+export default function({ route, store, redirect, req }) {
+  const cookieStore = process.client
+    ? Cookies.getJSON('mai-score')
+    : cookie.parse(req.headers.cookie || '')['mai-score']
+  if (
+    (store.state.user.isAuthenticatedFlg && store.state.user.uid) ||
+    (cookieStore.user.isAuthenticatedFlg && cookieStore.user.uid)
+  ) {
     if (
       route.name === 'login' ||
       route.name === 'regist' ||
