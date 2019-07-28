f4bd6a1957848451125fd200089c4d16529b824b
mnao305 <naomasa305@gmail.com>
2019-07-28 11:06:04 +0900

    CookieのKeyの変更
    こうしないとServer側で動かないらしい

--- client/plugins/localStorage.ts
+++ client/plugins/localStorage.ts
@@ -4,14 +4,14 @@ import cookie from 'cookie'
 
 export default ({ store, req, isDev }) => {
   createPersistedState({
-    key: 'mai-score',
+    key: '__session',
     storage: {
       getItem: (key) =>
         process.client
           ? Cookies.getJSON(key)
           : cookie.parse(req.headers.cookie || '')[key],
       setItem: (key, value) =>
-        Cookies.set(key, value, { expires: 365, secure: !isDev }), // 365日間 cookie storage を保持する
+        Cookies.set(key, value, { expires: 30, secure: !isDev }),
       removeItem: (key) => Cookies.remove(key)
     }
   })(store)
