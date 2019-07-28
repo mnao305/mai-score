bb4c26b726532f98d0202a876207cfaad1d39610
mnao305 <naomasa305@gmail.com>
2019-07-28 11:06:28 +0900

    サーバ側でもStore扱えるようになったので変更

--- client/middleware/authenticated.ts
+++ client/middleware/authenticated.ts
@@ -1,12 +1,5 @@
-export default async function({ route, store, redirect, app }) {
-  const cookieStore = await app.$cookies.get('mai-score')
-
-  if (
-    (cookieStore &&
-      cookieStore.user &&
-      (cookieStore.user.isAuthenticatedFlg && cookieStore.user.uid)) ||
-    (store.state.user.isAuthenticatedFlg && store.state.user.uid)
-  ) {
+export default function({ route, store, redirect }) {
+  if (store.state.user.isAuthenticatedFlg && store.state.user.uid) {
     if (
       route.name === 'login' ||
       route.name === 'regist' ||
@@ -15,7 +8,7 @@ export default async function({ route, store, redirect, app }) {
       redirect('/myscore')
     }
   } else {
-    // store.dispatch('user/logout')
-    // if (route.name === 'myscore') redirect('/')
+    store.dispatch('user/logout')
+    if (route.name === 'myscore') redirect('/')
   }
 }
