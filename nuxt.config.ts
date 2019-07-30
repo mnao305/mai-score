53f30b5f71a5b43a6b158748260b73c54ba925ec
mnao305 <naomasa305@gmail.com>
2019-07-30 11:49:18 +0900

    Vuetify2.0対応

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,3 @@
-import colors from 'vuetify/es5/util/colors'
 import NuxtConfiguration from '@nuxt/config'
 require('dotenv').config()
 
@@ -80,14 +79,9 @@ const config: nuxtConfg = {
    ** https://github.com/nuxt-community/vuetify-module
    */
   vuetify: {
+    customVariables: ['~/assets/variables.scss'],
     theme: {
-      primary: colors.blue.darken2,
-      accent: colors.grey.darken3,
-      secondary: colors.amber.darken3,
-      info: colors.teal.lighten1,
-      warning: colors.amber.base,
-      error: colors.deepOrange.accent4,
-      success: colors.green.accent3
+      dark: false
     }
   },
 
