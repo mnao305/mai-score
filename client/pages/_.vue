c2934fea38c05e1420c19d01f821ce9afdeb5393
mnao305 <naomasa305@gmail.com>
2019-07-31 14:13:10 +0900

    存在しないページに行くとトップに飛ばす。

---
+++ client/pages/_.vue
@@ -0,0 +1,9 @@
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+@Component({
+  fetch({ redirect }) {
+    redirect('/')
+  }
+})
+export default class defaultPage extends Vue {}
+</script>
