d7bc3e8cd207b867d03c75e77d147aabd969228a
mnao305 <naomasa305@gmail.com>
2019-07-31 14:30:27 +0900

    /user/でエラーになるのを修正

---
+++ client/pages/user/index.vue
@@ -0,0 +1,9 @@
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+@Component({
+  fetch({ redirect }) {
+    redirect('/')
+  }
+})
+export default class userDefaultPage extends Vue {}
+</script>
