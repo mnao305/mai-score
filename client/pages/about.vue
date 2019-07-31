ba3176fb4fa34d8db27e11f8a7f4ec206ef2fe8c
mnao305 <naomasa305@gmail.com>
2019-07-31 15:44:23 +0900

    Googleアナリティクスのことを書いた

--- client/pages/about.vue
+++ client/pages/about.vue
@@ -29,10 +29,35 @@
       当サイトはベータ版です。予告なくサービスの停止、データの削除等をおこなう可能性があります。<br />
       また、当アプリを利用し、利用者にいかなる損害が発生した場合も責任を負いかねます。
     </p>
+    <h5 class="headline">プライバシーポリシー</h5>
+    <p>
+      本サイトでは、サイトの分析と改善のためにGoogleアナリティクスを使用しています。<br />
+      その際、データ収集のためにGoogle がお使いのブラウザに cookie
+      を設定したり、既存のcookieを読み取ったりする場合があります。また、本サイトをご利用中のウェブブラウザは、Googleに特定の非個人情報（たとえば、アクセスしたページのウェブ
+      アドレスや IP アドレスなど）を自動的に送信します。<br />
+      当サイトはそれらの情報を、サイト利用状況の把握やサイト利用体験の改善、サイトユーザーの傾向をコンテンツとして紹介するといった用途で利用する可能性があります。
+    </p>
+    <p>
+      本サイトのユーザーは本サイトを利用することで、上記方法および目的においてGoogleとサイト管理者が行うこうしたデータ処理に対して、許可を与えたものとみなします。
+    </p>
+    <p>
+      Google社によるアクセス情報の収集方法および利用方法については、<a
+        href="https://marketingplatform.google.com/about/analytics/terms/jp/"
+        target="_blank"
+        >Google Analyticsサービス利用規約</a
+      >および<a
+        href="https://policies.google.com/privacy?hl=ja&gl=jp"
+        target="_blank"
+        >Google社プライバシーポリシー</a
+      >によって定められています。
+    </p>
     <h5 class="headline">リンク</h5>
     <p>
-      <a href="https://twitter.com/mnao_305">作者Twitter</a><br />
-      <a href="https://github.com/mnao305/mai-score">GitHubリポジトリ</a>
+      <a href="https://twitter.com/mnao_305" target="_blank">作者Twitter</a
+      ><br />
+      <a href="https://github.com/mnao305/mai-score" target="_blank"
+        >GitHubリポジトリ</a
+      >
     </p>
   </div>
 </template>
